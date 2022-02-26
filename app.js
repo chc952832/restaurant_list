8ca793882d4a3d4781bd6223b5c62688b82bfe72
chc952832 <littlekyuelf@gmail.com>
2022-02-26 22:11:41 +0800

    Add show.handlebars and setting show route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')  // require express-handlebars
+const restaurantList = require('./restaurant.json')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -13,9 +14,14 @@ app.use(express.static('public'))
 
 // 設定路由
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results})
 })
 
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  console.log(req.params.restaurant_id)
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', {restaurant: restaurant})
+})
 // 啟動並監聽伺服器
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
