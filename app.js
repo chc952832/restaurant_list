6b42de42f3fa225956837156b7944b50dd2038cd
chc952832 <littlekyuelf@gmail.com>
2022-02-26 23:12:26 +0800

    Setting search route

--- app.js
+++ app.js
@@ -14,14 +14,25 @@ app.use(express.static('public'))
 
 // 設定路由
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results})
+  res.render('index', { restaurants: restaurantList.results })
 })
 
+// 設定show頁面路由
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  console.log(req.params.restaurant_id)
+  // 用params和find方, 比對餐廳清單找到使用者點選的餐廳
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', {restaurant: restaurant})
+  res.render('show', { restaurant: restaurant })
 })
+
+// 設定搜尋頁面路由
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase().trim()
+  const restaurants = restaurantList.results.filter(restaurant => 
+    (restaurant.name.trim().includes(keyword)) || 
+    (restaurant.category.trim().includes(keyword)))
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
 // 啟動並監聽伺服器
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
